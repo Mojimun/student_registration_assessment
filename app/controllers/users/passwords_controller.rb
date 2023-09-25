d646695b736ed8b92d7c301c06ecf1e762f51940
Mojimun <mojimunislam@gmail.com>
2023-09-25 20:17:12 +0530

    sign up, login, profile view fix

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -20,7 +20,15 @@ class Users::PasswordsController < Devise::PasswordsController
   # def update
   #   super
   # end
-
+  def update
+    # This action handles the form submission and changes the password
+    if current_user.update_with_password(password_params)
+      bypass_sign_in(current_user)
+      redirect_to root_path, notice: 'Your password has been changed successfully.'
+    else
+      render :edit
+    end
+  end
   # protected
 
   # def after_resetting_password_path_for(resource)
@@ -31,4 +39,7 @@ class Users::PasswordsController < Devise::PasswordsController
   # def after_sending_reset_password_instructions_path_for(resource_name)
   #   super(resource_name)
   # end
+  def password_params
+    params.require(:user).permit(:current_password, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true
   validates :dob, presence: true
   validates :address, presence: true
